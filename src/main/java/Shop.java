f13603447e187f114d9c2de2a2a968dde5017cf9
Jo Malo <juanitamalo.p@gmail.com>
2018-05-27 10:36:39 +0100

    refactoring: removed unnecessary extra arraylist from shop

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -3,25 +3,11 @@ import Interfaces.ISell;
 import java.util.ArrayList;
 
 public class Shop {
-//    private ArrayList<Accessories> stockTypes;
     private ArrayList<ISell> stockList;
 
     public Shop() {
-//        stockTypes = new ArrayList<>();
         stockList = new ArrayList<>();
     }
-
-//    public int getShopInventoryCount() {
-//        return this.stockTypes.size();
-//    }
-//
-//    public void addStockToShopInventoryCount(Accessories accessories) {
-//        this.stockTypes.add(accessories);
-//    }
-//
-//    public void removeStockFromShopInventory(Accessories accessories) {
-//        this.stockTypes.remove(0);
-//    }
 
     public int getIstockCount() {
         return this.stockList.size();
@@ -31,15 +17,11 @@ public class Shop {
         this.stockList.add(stock);
     }
 
-    public void removeStockFromIstock(Accessories accessories) {
-        this.stockList.remove(0);
-    }
-
     public void addToIstock(Instrument instrument) {
         this.stockList.add(instrument);
     }
 
-    public int getIstockInventoryCount() {
-        return this.stockList.size();
+    public void removeStockFromIstock(Accessories accessories) {
+        this.stockList.remove(0);
     }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -16,44 +16,33 @@ public class ShopTest {
         shop = new Shop();
     }
 
-//    @Test
-//    public void canGetShopInventoryCount() {
-//        assertEquals(0, shop.getIstockCount());
-//    }
-
-//    @Test
-//    public void canAddStockToShopInventory() {
-//        shop.addStockToShopInventoryCount(accessories);
-//        assertEquals(1, shop.getShopInventoryCount());
-//    }
-
     @Test
     public void canGetIstockInventoryCount() {
-        assertEquals(0, shop.getIstockInventoryCount());
+        assertEquals(0, shop.getIstockCount());
     }
 
     @Test
     public void canAddSheetmusicToIstock() {
         shop.addToIstock(sheetMusic);
-        assertEquals(1, shop.getIstockInventoryCount());
+        assertEquals(1, shop.getIstockCount());
     }
 
     @Test
     public void canAddPianoToIstock() {
         shop.addToIstock(piano);
-        assertEquals(1, shop.getIstockInventoryCount());
+        assertEquals(1, shop.getIstockCount());
     }
 
     @Test
     public void canAddInstrumentToIstock() {
         shop.addToIstock(instrument);
-        assertEquals(1, shop.getIstockInventoryCount());
+        assertEquals(1, shop.getIstockCount());
     }
 
     @Test
     public void canAddGuitarToIstock() {
         shop.addToIstock(guitar);
-        assertEquals(1, shop.getIstockInventoryCount());
+        assertEquals(1, shop.getIstockCount());
     }
 
     @Test
